a6da987f12cfdf256b382b641869b17497e80bc8
Evgenii Shcherbakov <iipekolict@gmail.com>
2023-06-01 01:57:45 +0400

    fix: add cors local expo support

--- server/src/configs/cors.config.ts
+++ server/src/configs/cors.config.ts
@@ -3,9 +3,13 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 type StaticOrigin = boolean | string | RegExp | (string | RegExp)[];
 
 const originMatcher: CorsOptions['origin'] = (
-  origin: string,
+  origin: string | undefined,
   callback: (error: Error | null, origin?: StaticOrigin) => void,
 ) => {
+  if (origin === undefined) {
+    return callback(null, true);
+  }
+
   if (!origin || !origin.startsWith('http://localhost')) {
     return callback(new Error(`Request from origin '${origin}' is not allowed by CORS`));
   }
