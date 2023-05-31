607746a01078380903b0fcc6895a52b08598c57d
Evgenii Shcherbakov <iipekolict@gmail.com>
2023-05-31 18:06:18 +0400

    feat: add support for any port for server CORS configuration

---
+++ server/src/configs/cors.config.ts
@@ -0,0 +1,19 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+type StaticOrigin = boolean | string | RegExp | (string | RegExp)[];
+
+const originMatcher: CorsOptions['origin'] = (
+  origin: string,
+  callback: (error: Error | null, origin?: StaticOrigin) => void,
+) => {
+  if (!origin || !origin.startsWith('http://localhost')) {
+    return callback(new Error(`Request from origin '${origin}' is not allowed by CORS`));
+  }
+
+  callback(null, true);
+};
+
+export const corsOptions: CorsOptions = {
+  origin: originMatcher,
+  credentials: true,
+};
