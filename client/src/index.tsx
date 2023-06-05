303747f16e91373a378d397036dcbbd8d18e0460
Evgenii Shcherbakov <iipekolict@gmail.com>
2023-06-06 01:43:15 +0400

    feat: trigger client deploy

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,17 @@
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./App";
-import { setupStore } from "./store/store";
-import "./styles/index.scss";
-import "@fontsource/russo-one";
-import "@fontsource/montserrat";
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import { setupStore } from './store/store';
+import './styles/index.scss';
+import '@fontsource/russo-one';
+import '@fontsource/montserrat';
 import 'react-photo-view/dist/react-photo-view.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 const store = setupStore();
+
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 );
